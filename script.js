7cebfeb7e048356f91fde429a748de5c9d62cb5e
tiffany <yang.ding@hotmail.com>
2020-09-20 22:56:14 -0700

    finished js portion

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const container = document.getElementById("game-board");
 const boardSpaces = document.getElementsByClassName("board-space");
 
+const addCSS = (item) => {
+    item.style.cursor = "default";
+    item.style.animationName = "text";
+    item.style.animationDuration = "0.5s";
+    item.style.pointerEvents = "none";
+    
+    if (item.textContent == "X") {
+        item.style.color = "#3DBBFE";
+    } else {
+        item.style.color = "#F49CC8";
+    };
+}
+
 // module to initialize game board display
 const displayGameBoard = (() => {
     for (i = 0; i < 3; i++) {
@@ -17,7 +30,7 @@ const displayGameBoard = (() => {
 })();
 
 // module to initialize game board array
-const Board = (() => {
+const Board = () => {
     let gameBoard = []; 
 
     // initiate gameBoard with indices matching button data index
@@ -31,79 +44,161 @@ const Board = (() => {
         gameBoard.splice(index, 1);
     }
 
-    return {gameBoard, removeIndex};
-})(); 
+    const checkIfWon = (mark) => {
+        let board = [];
+        for (i = 0; i < boardSpaces.length; i++) {
+            board.push(boardSpaces[i].textContent);
+        }
+
+        if (rowWin(board, mark) || colWin(board, mark) || diagonalWin(board, mark)) {
+            return true;
+        };
+    }
+
+    const checkIfTie = () => {
+        // check if tie 
+        let board = [];
+        for (i = 0; i < boardSpaces.length; i++) {
+            board.push(boardSpaces[i].textContent);
+        }
+        for (i = 0; i < board.length; i++) {
+            if (board[i] == "") {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+    const rowWin = (board, mark) => {
+        const isEqual = (ele) => ele == mark;
+        for (i = 0; i < boardSpaces.length; i+=3) {
+            let row = board.slice(i, i+3);
+            if (row.every(isEqual)) {
+                return true; 
+            } 
+        }
+    }
+
+    const colWin = (board, mark) => {
+        const isEqual = (ele) => ele == mark;
+        for (i = 0; i < 3; i++) {
+            let col = [board[i], board[i+3], board[i+6]];
+            if (col.every(isEqual)) {
+                return true;
+            } 
+        }
+    }
+
+    const diagonalWin = (board, mark) => {
+        const isEqual = (ele) => ele == mark;
+        let diagonals = "";
+        for (i = 0; i < 3; i++) {
+            if (i == 0) {
+                diagonals = [board[i], board[i+4], board[i+8]];
+            } else if (i == 1) {
+                continue;
+            } else if (i == 2) {
+                diagonals = [board[i], board[i+2], board[i+4]];
+            }
+            if (diagonals.every(isEqual)) {
+                return true;
+            }
+        }
+    }
+
+    return {gameBoard, removeIndex, checkIfWon, checkIfTie};
+}; 
 
 const Player = (name, mark) => {
-    return {name, mark};
+    const makeMove = (board, boardSpace) => {
+        boardSpace.textContent = mark;
+        board.removeIndex(board.gameBoard.indexOf(boardSpace.dataset.index));
+        addCSS(boardSpace);
+    }
+    return {name, mark, makeMove};
 }
 
 const CompPlayer = (name, mark) => {
     // place mark on any current available index
-    const makeMove = () => {
-        const max = Board.gameBoard.length;
+    const makeMove = (board) => {
+        const max = board.gameBoard.length;
         
-        // generate a random number between 0 and max array length
+        // generate random position for computer
         let randomIndex = Math.floor(Math.random() * max);
-
-        let compChoice = Board.gameBoard[randomIndex];
+        let compChoice = board.gameBoard[randomIndex];
         
         // place marker
         for (i = 0; i < boardSpaces.length; i++) {
             let currentIndex = boardSpaces[i].dataset.index;
             if (compChoice == currentIndex) {
                 boardSpaces[i].textContent = mark;
-                boardSpaces[i].style.cursor = "default";
-                boardSpaces[i].classList.add("selected");
+                addCSS(boardSpaces[i]);
             }
         }
         // remove computer choice from gameboard array
-        Board.removeIndex(Board.gameBoard.indexOf(compChoice));
+        board.removeIndex(board.gameBoard.indexOf(compChoice));
 
     };
     return {name, mark, makeMove};
 }
 
-const Game = () => {
+const Game = (() => {
     // initiate players and board
     let player = Player("Player", "X");
     let computer = CompPlayer("Computer", "O");
-    let board = Board.gameBoard;
+    let board = Board();
 
     // human player makes first move 
     let currentPlayer = player;
-    
-    container.addEventListener("click", (e) => {
+
+    const runGame = (e) => {
         // player makes move 
         if (currentPlayer == player) {
-            let flag = true;
-            while (flag) {
-                if (e.target.textContent == "") {
-                    // if space is empty, place marker, adjust board, and switch turns
-                    let boardSpace = e.target;
-                    boardSpace.textContent = currentPlayer.mark;
-                    Board.removeIndex(board.indexOf(boardSpace.dataset.index));
-                    currentPlayer = computer;
-                    boardSpace.classList.add("selected");
-                    boardSpace.style.cursor = "default";
-                    flag = false;
-                } else {
-                    alert("This space is already taken!");
-                    return;
+            if (e.target.textContent == "") {
+                // if space is empty, place marker, adjust board, and switch turns
+                let boardSpace = e.target;
+                player.makeMove(board, boardSpace);
+                if (board.checkIfWon(player.mark)) {
+                    setTimeout(function() {
+                        alert(`${player.name} is the winner!`);
+                        resetGame();
+                    }, 400);
+                
                 };
+                currentPlayer = computer;
             }
-        } 
-
-        // computer makes move 
+        }
         setTimeout(function() {
-            computer.makeMove();
-        }, 600);
+            computer.makeMove(board);
+            if (board.checkIfWon(computer.mark)) {
+                setTimeout(function () {
+                    alert(`${computer.name} is the winner!`);
+                    resetGame();
+                }, 400);
+            };   
+        }, 800);
+
+        // switch player turn
         currentPlayer = player;
 
-        // alternate until board is filled 
-            // check who won
-            // or if tie
-    });
-}
+        if (!board.checkIfWon(player.mark) && !board.checkIfWon(computer.mark)) {
+            if (board.checkIfTie()) {
+                setTimeout(function() {
+                    alert("is tie");
+                    resetGame();
+                }, 500);
+            }
+        }
+    };
 
-let game = Game();
+    for (i = 0; i < boardSpaces.length; i++) {
+        boardSpaces[i].addEventListener("click", runGame)
+    }
+
+
+})();
+
+const resetGame = () => {
+    location.reload();
+}
